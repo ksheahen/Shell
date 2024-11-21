1f09ae1e92769a46722024f11141a7d6c110a57d
Sheahen, Kathryn <112595660+ksheahen@users.noreply.github.com>
2024-11-21 16:19:56 -0600

    path input size fixed

--- interactive.c
+++ interactive.c
@@ -10,8 +10,8 @@
 
 void interactive() {
     bool running = true;    //flag
-    char *cmd[100];         //linux commands
-    char input[30];         //user input
+    char *cmd[512];         //linux commands
+    char input[512];         //user input
     int i = 0;              //index
 
     while (running) {
@@ -30,53 +30,10 @@ void interactive() {
             break;
         }
 
-
-
-
-        // char *s = (char *)malloc(100 * sizeof(char)); //path 
-
-        // // Built-In Commands
-
-        // if (strcmp(input, "exit") == 0) {
-        //     // If the user input is "exit", exit the program.
-        //     printf("exiting shell\n");
-        //     exit(0);
-        
-        // } else if (strcmp(input, "cd") == 0) {
-        //     // If the user input is "cd", change the directory.
-        //     // printf("cd\n"); //debug
-
-        //     //doesn't work rn - need a no-arg function to go back to the previous directory
-        //     // error: seg fault
-        //     // if (strcmp(cmd[1], "1") == 0 ) {
-        //     //     chdir("..");
-        //     //     printf("new path: %s\n", getcwd(s, 100));
-        //     // }
-
-
-        //     //doesn't take a path with spaces rn
-        //     //cmd = cd /path/to/directory
-        //     printf("original path: %s\n", getcwd(s, 100)); //100 should be the size of the buffer
-        //     chdir(cmd[1]);
-        //     printf("new path: %s\n", getcwd(s, 100));
-            
-        // } else if (strcmp(input, "path") == 0) {
-        //     // If the user input is "path", allows users to show the current
-        //     // pathname list, append one pathname, or remove one pathname
-        //     printf("path\n");
-            
-        // } else if (strcmp(input, "myhistory") == 0) {
-        //     // If the user input is "myhistory", list the shell history.
-        //     printf("myhistory\n");
-
-
-
         if (builtInCommands(input) == -1) {
             continue; 
         } else {
-          
-                        // // Reset the index
-            i = 0;
+
 
             // Seperate the command and the arguments entered
             // by the user
