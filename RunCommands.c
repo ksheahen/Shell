bf703e34b3ad7f602834b4379700466a5baca823
Sheahen, Kathryn <112595660+ksheahen@users.noreply.github.com>
2024-11-30 22:03:10 -0600

    piping

--- RunCommands.c
+++ RunCommands.c
@@ -187,6 +187,154 @@ int redirection(char* command, int isInputRedirection, int isOutputRedirection) 
 // Pipes
 int pipeline(char* command) {
 
+    char** pipes = SplitCommandsWithPipes(command); //split the commands with pipes
+    int counter = 0;
+    int commands = 0;
+    char **commandArgs;
+    char **commandArgs2;
+    char **commandArgs3;
+    int child1, child2, child3;
+    int pipeOne, pipeTwo;
+
+    // Count the number of commands seperated by pipes
+    while(pipes[counter] != NULL) {
+        counter++;
+        commands++;
+    }
+
+    switch (commands) {
+
+        // If there are two commands (one pipe), execute case 2.
+        case 2:
+            // Split the two commands with their respective arguments
+            commandArgs = splitCommandsInArguments(pipes[0]);
+            commandArgs2 = splitCommandsInArguments(pipes[1]);
+
+            int fd[2]; //file descriptor
+            pipeOne = pipe(fd); //create a pipe
+
+            // Check for error creating the pipe
+            if (pipeOne == -1) {
+                printf("Error creating pipe\n");
+                return -1;
+            }
+
+            // Execute the first command
+            if ((child1 = fork()) == 0) {
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+
+                if (execvp(commandArgs[0], commandArgs) == -1) {
+                    printf("%s: command not found\n", commandArgs[0]);
+                    exit(1);
+                }
+            }
+
+            // Execute the second command
+            if ((child2 = fork()) == 0) {
+                close(fd[1]);
+                dup2(fd[0], STDIN_FILENO);
+                close(fd[0]);
+
+                if (execvp(commandArgs2[0], commandArgs2) == -1) {
+                    printf("%s: command not found\n", commandArgs2[0]);
+                    exit(1);
+                }
+            }
+
+            // Close the file descriptors
+            close(fd[0]);
+            close(fd[1]);
+
+            // Wait for the child processes to finish
+            waitpid(child1, NULL, 0);
+            waitpid(child2, NULL, 0);
+
+            break;
+        // If there are three commands (two pipes), execute case 3.
+        case 3:
+            // Split the three commands with their respective arguments
+            commandArgs = splitCommandsInArguments(pipes[0]);
+            commandArgs2 = splitCommandsInArguments(pipes[1]);
+            commandArgs3 = splitCommandsInArguments(pipes[2]);
+
+            // File descriptors
+            int fd1[2]; 
+            int fd2[2];
+
+            // Create the pipes
+            pipeOne = pipe(fd1);
+            pipeTwo = pipe(fd2);
+
+            // Check for error creating the pipes
+            if (pipeOne == -1 || pipeTwo == -1) {
+                printf("Error creating pipe\n");
+                return -1;
+            }
+
+            // Execute the first command
+            if ((child1 = fork()) == 0) {
+                dup2(fd1[1], STDOUT_FILENO);
+                close(fd1[0]);
+                close(fd1[1]);
+                close(fd2[0]);
+                close(fd2[1]);
+
+                if (execvp(commandArgs[0], commandArgs) == -1) {
+                    printf("%s: command not found\n", commandArgs[0]);
+                    exit(1);
+                }
+            }
+
+            // Execute the second command
+            if ((child2 = fork()) == 0) {
+                dup2(fd1[0], STDIN_FILENO);
+                dup2(fd2[1], STDOUT_FILENO);
+                close(fd1[0]);
+                close(fd1[1]);
+                close(fd2[0]);
+                close(fd2[1]);
+
+                if (execvp(commandArgs2[0], commandArgs2) == -1) {
+                    printf("%s: command not found\n", commandArgs2[0]);
+                    exit(1);
+                }
+            }
+
+            // Execute the third command
+            if ((child3 = fork()) == 0) {
+                dup2(fd2[0], STDIN_FILENO);
+                close(fd1[0]);
+                close(fd1[1]);
+                close(fd2[0]);
+                close(fd2[1]);
+
+                if (execvp(commandArgs3[0], commandArgs3) == -1) {
+                    printf("%s: command not found\n", commandArgs3[0]);
+                    exit(1);
+                }
+            }
+
+            // Close the file descriptors
+            close(fd1[0]);
+            close(fd1[1]);
+            close(fd2[0]);
+            close(fd2[1]);
+
+            // Wait for the child processes to finish
+            waitpid(child1, NULL, 0);
+            waitpid(child2, NULL, 0);
+            waitpid(child3, NULL, 0);
+            
+            break;
+
+        // If there are more than three commands (3 or more pipes), print an error message.
+        default:
+            printf("Error: too many pipes\n");
+            return -1;
+
+    }
 
     return 0;
 }
