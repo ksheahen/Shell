af02c4476b8d46f3938b748226e7bc3149c04e83
Kathryn Sheahen <112595660+ksheahen@users.noreply.github.com>
2025-03-12 14:49:01 -0500

    myhistory -e added

--- myhistory.c
+++ myhistory.c
@@ -8,18 +8,19 @@ int addToHistory(char* command) {
     if (counter == 0) {
         history = malloc(100 * sizeof(char*));
     }
-
+    
     // Allocate memory for the command and copy it to the history array.
     history[counter] = malloc((strlen(command) + 1) * sizeof(char));
     strcpy(history[counter], command);
     counter++;
-
     return 0;
 }
 
 int myCommandHistory(char** commandArgs) {
 
     char *args = (char *)malloc(100 * sizeof(char));
+    char *cmdNum = (char *)malloc(100 * sizeof(char));
+    cmdNum = commandArgs[2];
     args[0] = '\0';
     int i=1;
 
@@ -47,10 +48,15 @@ int myCommandHistory(char** commandArgs) {
     } else if (strcmp(args, "") == 0) {
         //printf("history print"); //debug
         for (int i = 0; i < counter; i++) {
-            printf("%s\n", history[i]);
+            int num = i + 1;
+            printf("%d: %s\n", num, history[i]);
         }
+    } else if (strstr(args, "-e")) {
+        // printf("flag e "); //debug
+        int index = atoi(cmdNum) - 1;
+        runCommands(history[index]);
     }
-
+    
     free(args);
     return 0;
 }
